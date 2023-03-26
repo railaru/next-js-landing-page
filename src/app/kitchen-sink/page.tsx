d34dacab2303357b55d0f8396a6d24c9f8be258e
rutenisraila <rutenisraila@hotmail.com>
2023-03-26 16:33:28 +0300

    setup next.js, tailwind and next/font

---
+++ src/app/kitchen-sink/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main className="space-y-4">
+      <h3 className="font-bold">Font weight 700</h3>
+      <h3 className="font-[500]">Font weight 500</h3>
+      <h3 className="font-[400]">Font weight 400</h3>
+      <h3 className="font-[300]">Font weight 300</h3>
+    </main>
+  );
+}
